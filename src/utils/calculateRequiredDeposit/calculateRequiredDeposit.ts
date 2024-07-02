96c195634de922990c0d2a325c19af62bc7ed31e
Kieran O'Neill <hello@kieranoneill.com>
2024-07-02 15:05:19 +0100

    chore: squash

--- src/utils/calculateRequiredDeposit/calculateRequiredDeposit.ts
+++ src/utils/calculateRequiredDeposit/calculateRequiredDeposit.ts
@@ -11,11 +11,11 @@ import type { IOptions } from './types';
 
 /**
  * Calculates the deposit required for storage. If the storage balance is not available, a minimum storage cost is
- * required. However, if the cost of the data exceeds the minimum, the cost of the data is used. In the instances that
- * there is storage balance available, the deposit is determined based on the difference between the available and the
- * required amount.
+ * required. However, if the cost of the data exceeds the minimum storage cost, the cost of the data is used. In the
+ * instances that there is storage balance available, the deposit is determined based on the difference between the
+ * available and the required amount.
  * @param {IOptions} options - the data to be stored and the storage balance.
- * @returns {BigNumber} the required deposit
+ * @returns {BigNumber} the required deposit.
  */
 export default function calculateRequiredDeposit({
   data,
@@ -29,7 +29,7 @@ export default function calculateRequiredDeposit({
   ).multipliedBy(STORAGE_COST_PER_BYTES_IN_ATOMIC_UNITS);
   let storageDepositAvailable: BigNumber;
 
-  // if there is no balance, use the minimum storage cost, or the storage cost of the data.
+  // if there is no balance, use the minimum storage cost, or the storage cost of the data
   if (!storageBalance) {
     return storageCostOfData.lt(minimumStorageCost)
       ? minimumStorageCost
@@ -38,7 +38,7 @@ export default function calculateRequiredDeposit({
 
   storageDepositAvailable = new BigNumber(storageBalance.available.toString());
 
-  // if the storage deposit available is less than the cost of storage, deposit the difference required
+  // if the storage deposit available is less than the cost of storage, use the difference as the required deposit
   return storageDepositAvailable.lt(storageCostOfData)
     ? storageCostOfData.minus(storageDepositAvailable)
     : new BigNumber('1');
