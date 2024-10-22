363ac15961910ab47b8da71b3ef97d26bbca94c6
Elliot Braem <elliot@ejlbraem.com>
2024-10-21 23:53:01 -0400

    fix: bundle dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   outDir: 'dist', // output directory
   format: ['esm', 'cjs'], // CommonJS and ESM
   // Exclude certain files and directories
-  external: ['docs', 'test', 'node_modules', 'src/**/*.test.ts'],
+  bundle: true,
+  external: ['docs', 'test', 'src/**/*.test.ts'],
   sourcemap: true, // for easier debugging
   dts: true, // Generate declaration files
   clean: true, // Clean output directory before build
